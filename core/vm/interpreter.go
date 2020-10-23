162abbd442748fad4c63a439f48b8a1ebfaca8ba
yuto51942 <yuto.w51942@gmail.com>
2020-10-23 10:16:34 +0900

    remove check print message.

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -17,7 +17,6 @@
 package vm
 
 import (
-	"fmt"
 	"hash"
 	"sync/atomic"
 
@@ -296,8 +295,6 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		case err != nil:
 			return nil, err
 		case operation.reverts:
-			fmt.Println(op)
-			fmt.Println(operation)
 			return res, ErrExecutionReverted
 		case operation.halts:
 			return res, nil
